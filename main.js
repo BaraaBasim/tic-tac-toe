9f043f6aac0f46bff1285c46b775e2e25f8c42cd
Baraa Basim <barabasim77@gmail.com>
2023-10-14 19:09:00 +0800

    board

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function createPlayer() {
+function player(mark) {
+  let _mark = mark;
   return {};
 }
 
 const gameBoard = (function () {
-  let board = [];
+  let board = new Array(9);
+  function setMark(player, mark) {}
 })();
 
 const displayController = (function () {})();
