60f75be40135b24dc410bc0a78f07e154bedcbb9
Baraa Basim <barabasim77@gmail.com>
2023-10-24 18:31:38 +0800

    click handler and switch turn

--- main.js
+++ main.js
@@ -1,42 +1,20 @@
-function player(mark) {
-  let _mark = mark;
-  let turn = false;
-  return {
-    mark,
-    turn,
-  };
+const cells = document.querySelectorAll(".cell");
+let playerTurn = "x";
+cells.forEach((cell) => {
+  cell.addEventListener("click", clickHandler, { once: true });
+});
+
+function clickHandler(e) {
+  e.target.classList.add(playerTurn);
+  switchTurn();
 }
 
-const gameBoard = (function () {
-  let _board = new Array(9);
-  const cellElements = document.querySelectorAll(".cell");
-  cellElements.forEach((cell) => {
-    cell.addEventListener("click", handleClick);
-  });
-})();
-
-const gameController = (() => {
-  function switchTurn() {
-    const board = document.getElementById("board");
-    if (board.classList.contains("x")) {
-      board.classList.remove("x");
-      board.classList.add("o");
-    } else if (board.classList.contains("o")) {
-      board.classList.remove("o");
-      board.classList.add("x");
-    }
+function switchTurn() {
+  if (playerTurn === "x") {
+    playerTurn = "o";
+  } else if (playerTurn === "o") {
+    playerTurn = "x";
   }
+}
 
-  return {
-    getHumanPlayer,
-    getAiPlayer,
-    checkForWin,
-    checkForDraw,
-    switchTurn,
-    playerStep,
-    endGame,
-    restart,
-  };
-})();
-
-const displayController = () => {};
+function checkWin() {}
